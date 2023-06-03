90841a643fe38b0a29ffafd872dcc6ecbcf757a6
Tre <tregallowayofficial@gmail.com>
2023-06-03 17:32:13 -0500

    trying out layout for home-header

--- src/components/cards/home-header.svelte
+++ src/components/cards/home-header.svelte
@@ -1,5 +1,6 @@
 <script>
 	export const ProfileImage = '';
+	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
 <main>
@@ -8,11 +9,18 @@
 			<div class="flex grow">
 				<section class="card w-full">
 					<div class="p-4 space-y-4">
-						<div class="placeholder animate-pulse" />
-						<div class="grid grid-cols-3 gap-8">
-							<div class="placeholder animate-pulse" />
-							<div class="placeholder animate-pulse" />
-							<div class="placeholder animate-pulse" />
+						<h1 class="h1">
+							<span
+								class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+								>Tre Galloway.</span
+							>
+						</h1>
+						<div class="grid grid-cols-3 gap-10">
+							<span class="chip bg-gradient-to-br variant-gradient-secondary-primary"
+								>Fullstack Developer</span
+							>
+							<span class="chip bg-gradient-to-br from-red-500 to-yellow-500">Writer</span>
+							<span class="chip bg-gradient-to-br from-pink-500 to-violet-500">Overly Curious</span>
 						</div>
 						<div class="grid grid-cols-4 gap-4">
 							<div class="placeholder animate-pulse" />
@@ -23,7 +31,13 @@
 					</div>
 				</section>
 			</div>
-			<div class="p-2"><div class="placeholder-circle animate-pulse w-52" /></div>
+			<div class="p-2 pl-10">
+				<Avatar
+					src="https://source.unsplash.com/YOErFW8AfkI/128x128"
+					width=" w-48"
+					rounded="rounded-full"
+				/>
+			</div>
 		</div>
 	</div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,14 @@
 	import HomeHeader from '../components/cards/home-header.svelte';
 </script>
 
-<div class="container h-full mx-auto justify-center items-center">
-	<div class="card">
-		<header class="card-header">
-			<HomeHeader />
-		</header>
-		<section class="p-4">(content)</section>
-		<footer class="card-footer">(footer)</footer>
+<div class="mx-3">
+	<div class="container h-full mx-auto justify-center items-center">
+		<div class="card">
+			<header class="card-header">
+				<HomeHeader />
+			</header>
+			<section class="p-4">(content)</section>
+			<footer class="card-footer">(footer)</footer>
+		</div>
 	</div>
 </div>
